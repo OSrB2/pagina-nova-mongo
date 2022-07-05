a1c46a760010fc4c124b0675adf0de1eba6701b0
Pedro Oliveira <pedro_oliva@outlook.com.br>
2022-07-04 21:11:16 -0300

    feat: adding MongoDb connection

--- src/app.js
+++ src/app.js
@@ -1,8 +1,12 @@
-const express = require("express");
-const upload  = require('./config/upload')
-const routes = require("./routes");
-const handleError = require("./middlewares/handleError");
+const express = require('express');
+const upload = require('./config/upload');
+const routes = require('./routes');
+const handleError = require('./middlewares/handleError');
 const path = require('path');
+const connectMongoDb = require('./database');
+const mongoose = require('mongoose');
+
+connectMongoDb();
 
 const app = express();
 
@@ -13,11 +17,15 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(upload.any());
 
-app.use(express.static('src/public'))
+app.use(express.static('src/public'));
 
 app.use(routes);
 
 app.use(handleError);
 
-const port = process.env.PORT || 4010
-app.listen(port, () => console.log("Servidor rodando na porta 4010"));
+mongoose.connection.once('open', () => {
+  console.log('DB conectado!');
+
+  const port = process.env.PORT || 4010;
+  app.listen(port, () => console.log('Servidor rodando na porta 4010'));
+});
